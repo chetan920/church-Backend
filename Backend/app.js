d3cafdebfc27fe4b8f5b04fc5e030dddd74b8b9d
chetan920 <154891145+chetan920@users.noreply.github.com>
2024-10-22 19:54:35 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://christarmenianchurch.com/",
+    origin: "https://christarmenianchurch.com",
     credentials: true,
   })
 );
